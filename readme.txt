15462a2bfde77db577cba8c2b9ef6504307de88d
Alexey Kolesnikov <ak@blu-disc.net>
2021-04-08 17:37:59 +0300

    Universal version, added settings

--- readme.txt
+++ readme.txt
@@ -1,16 +1,22 @@
-Minimize Thunderbird to tray helper 0.5
+ThunderTray 1.0
+
+Minimize application to tray helper. Initially created for Thunderbird.
 
 1. Why?
-  - 88.0b1 crashes when restoring from tray and using "System Integration" -> "When Thunderbird is minimized, move it to the tray".
-  - In 88.0b1, the new message icon is not cleared if old messages are marked as unread.
+  - 88.0b1 crashed when restoring from tray and using "System Integration" -> "When Thunderbird is minimized, move it to the tray".
+  - Starting from 88.0b1, the new message icon is not cleared if old messages are marked as unread.
   - Thunderbird hides the tray icon when restoring.
 
 2. How to use
-  - Disable "System Integration" -> "When Thunderbird is minimized, move it to the tray"
-  - Disable "Incoming mails" -> "Show a tray icon"
-  - Enable "Incoming mails" -> "Show an alert" and set the alert for at least 5 secons (check the Customize button)
-  - Place in Thunderbird folder and run. If thunderbird.exe is not found, C:\Program Files\Mozilla Thunderbird\thunderbird.exe will start
+  - You can use ThunderTray.ini (see example in Thunderbird folder) or run with parameters:
+    - ThunderTray C:\App.exe Param1 /Param2 ...
+    - ThunderTray /SAFE C:\App.exe Param1 /Param2 ...
+    - use custom ThunderTray.ini: ThunderTray C:\App.ini
+    /SAFE means change EXSTYLE instead of calling ShowWindow
+  - To use with Thunderbird:
+    - Disable "System Integration" -> "When Thunderbird is minimized, move it to the tray"
+    - Disable "Incoming mails" -> "Show a tray icon"
+    - Enable "Incoming mails" -> "Show an alert" and set the alert for at least 5 secons (check the Customize button)
 
 3. TODO:
-  - Use SetWindowsHookEx
-  - Settings
+  ? Use SetWindowsHookEx (unfortunately SetWindowsHookEx(WH_CBT, ...) doesn't give HCBT_MINMAX on Win+D https://github.com/AlekseyKolesnikov/SetWindowsHookEx-demo)
--- uThunderTray.pas
+++ uThunderTray.pas
@@ -3,20 +3,28 @@ unit uThunderTray;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes, Vcl.Controls, Vcl.Forms, Vcl.ExtCtrls, System.ImageList, Vcl.ImgList;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes, Vcl.Controls, Vcl.Forms, Vcl.ExtCtrls, Vcl.Graphics,
+  System.ImageList, Vcl.ImgList, Vcl.Menus;
 
 type
   TfrmMain = class(TForm)
     Timer: TTimer;
     TrayIcon: TTrayIcon;
     ImageList: TImageList;
+    pmPopupMenu: TPopupMenu;
+    miShowHide: TMenuItem;
+    N1: TMenuItem;
+    miExit: TMenuItem;
     procedure HideTimer(Sender: TObject);
     procedure MonitorTimer(Sender: TObject);
     procedure TrayIconClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure miShowHideClick(Sender: TObject);
+    procedure miExitClick(Sender: TObject);
   private
     { Private declarations }
+    procedure InitSettings;
     procedure RunApp;
   protected
     procedure CreateParams(var Params: TCreateParams); override;
@@ -31,11 +39,31 @@ implementation
 
 {$R *.dfm}
 
+uses
+  IniFiles, Winapi.ShellAPI;
+
+const
+  LAUNCH_ERROR = $FFFFFFFF;
+
 var
   AppThread, WinHandle: Cardinal;
+  Settings: record
+    Safe, UsePopupMonitor, PopupStyleCheck, PopupExStyleCheck, PopupTitleEqual, MainTitleEqual, UpdateTitle: Boolean;
+    RunApp, Params, Folder, PopupTitle, MainTitle, IconTitle, IconMain, IconPopup: String;
+    StartTimeout, HideMonitorTimer, PopupMonitorTimer: Integer;
+    PopupStyleMask, PopupStyleValue, PopupExStyleMask, PopupExStyleValue: Cardinal
+  end;
+  PopupDetected: Boolean;
   {$IFDEF DEBUG}
   slDebug: TStringList;
   {$ENDIF}
+
+function AddPath(const S: string): string;
+begin
+  Result := S;
+  if (Result <> '') and (ExtractFilePath(Result) = '') then
+    Result := ExtractFilePath(Application.ExeName) + Result;
+end;
 
 function AddQuotes(const S: string): string;
 begin
@@ -45,7 +73,7 @@ begin
     Result := '"' + Result + '"';
 end;
 
-function InstExec(const Filename, Params: string; WorkingDir: string; const ShowCmd: Integer; out ErrorCode: Integer): Cardinal;
+function ExecuteApp(const Filename, Params: string; WorkingDir: string; const ShowCmd: Integer; out ErrorCode: Integer): Cardinal;
 var
   CmdLine: string;
   WorkingDirP: PChar;
@@ -53,7 +81,7 @@ var
   ProcessInfo: TProcessInformation;
   cWaitIdle: Cardinal;
 begin
-  CmdLine := AddQuotes(Filename) + ' ' + Params + #0 {needs null terminator for Delphi 1};
+  CmdLine := AddQuotes(Filename) + ' ' + Params + #0;
 
   if WorkingDir = '' then
     WorkingDir := ExtractFileDir(Filename);
@@ -70,23 +98,50 @@ begin
 
   if not CreateProcess(nil, PChar(CmdLine), nil, nil, False, 0, nil, WorkingDirP, StartupInfo, ProcessInfo) then
   begin
-    Result := $FFFFFFFF;
+    Result := LAUNCH_ERROR;
     ErrorCode := GetLastError;
     Exit;
   end;
 
-  with ProcessInfo do
+  CloseHandle(ProcessInfo.hThread);
+
+  WaitForInputIdle(ProcessInfo.hProcess, INFINITE);
+
+  repeat
+    cWaitIdle := WaitForInputIdle(ProcessInfo.hProcess, 50);
+  until (cWaitIdle <> WAIT_TIMEOUT);
+
+  Result := ProcessInfo.dwThreadId;
+  CloseHandle(ProcessInfo.hProcess);
+end;
+
+procedure InitDefaultSettings;
+begin
+  with Settings do
   begin
-    CloseHandle(hThread);
+    StartTimeout := 10;
+    HideMonitorTimer := 500;
+    PopupMonitorTimer := 2000;
+    Safe := False;
+    Params := '';
+    Folder := '';
+    RunApp := '';
+    IconTitle := '';
 
-    WaitForInputIdle(hProcess, INFINITE);
+    UpdateTitle := False;
+    MainTitle := '';
+    MainTitleEqual := False;
+    PopupTitle := '';
+    PopupTitleEqual := False;
 
-    repeat
-      cWaitIdle := WaitForInputIdle(hProcess, 50);
-    until (cWaitIdle <> WAIT_TIMEOUT);
+    UsePopupMonitor := False;
 
-    Result := dwThreadId;
-    CloseHandle(hProcess);
+    PopupStyleMask := $FFFFFFFF;
+    PopupStyleValue := 0;
+    PopupStyleCheck := False;
+    PopupExStyleMask := $FFFFFFFF;
+    PopupExStyleValue := 0;
+    PopupExStyleCheck := False;
   end;
 end;
 
@@ -94,17 +149,139 @@ procedure HideApp(wHandle: Cardinal);
 var
   style: Integer;
 begin
-  style := GetWindowLong(wHandle, GWL_EXSTYLE);
-  SetWindowLong(wHandle, GWL_EXSTYLE, style or WS_EX_TOOLWINDOW);
+  if Settings.Safe then
+  begin
+    style := GetWindowLong(wHandle, GWL_EXSTYLE);
+    SetWindowLong(wHandle, GWL_EXSTYLE, style or WS_EX_TOOLWINDOW);
+  end
+  else
+    ShowWindow(wHandle, SW_HIDE);
 end;
+
+procedure LoadSettings(sIniFile: String);
+var
+  ini: TIniFile;
+
+  function ReadBool(Section, Key: String; Default: Boolean): Boolean;
+  var
+    S: String;
+  begin
+    Result := Default;
+    S := AnsiLowerCase(Trim(ini.ReadString(Section, Key, '')));
+
+    if S = '' then
+      Exit;
+
+    Result := (S = 'true') or (S = 'yes') or (S = '1');
+  end;
+
+  function ReadCardinal(Section, Key: String; Default: Cardinal): Cardinal;
+  var
+    S: String;
+  begin
+    Result := Default;
+    S := AnsiLowerCase(Trim(ini.ReadString(Section, Key, '')));
+
+    if S = '' then
+      Exit;
+
+    S := StringReplace(S, '0x', '$', []);
+    try
+      Result := StrToUInt(S);
+    except
+    end;
+  end;
+
+begin
+  ini := TIniFile.Create(sIniFile);
+
+  with Settings do
+  begin
+    RunApp := Trim(ini.ReadString('Run', 'Path', ''));
+    if RunApp = '' then
+    begin
+      ini.Free;
+      Exit;
+    end;
+    RunApp := AddPath(RunApp);
+
+    Params := Trim(ini.ReadString('Run', 'Params', Params));
+    Folder := Trim(ini.ReadString('Run', 'Folder', Folder));
+    Safe := ReadBool('Run', 'Safe', Safe);
+
+    StartTimeout := ini.ReadInteger('Timers', 'StartTimeout', StartTimeout);
+    HideMonitorTimer := ini.ReadInteger('Timers', 'HideMonitorTimer', HideMonitorTimer);
+    PopupMonitorTimer := ini.ReadInteger('Timers', 'PopupMonitorTimer', PopupMonitorTimer);
+
+    MainTitleEqual := ReadBool('Main', 'MainTitleEqual', MainTitleEqual);
+    MainTitle := ini.ReadString('Main', 'MainTitle', MainTitle);
+    IconTitle := ini.ReadString('Main', 'IconTitle', IconTitle);
+    UpdateTitle := ReadBool('Main', 'UpdateTitle', UpdateTitle);
+
+    IconMain := AddPath(Trim(ini.ReadString('Main', 'IconMain', IconMain)));
+    IconPopup := AddPath(Trim(ini.ReadString('Main', 'IconPopup', IconPopup)));
+
+    UsePopupMonitor := ReadBool('Popup', 'UsePopupMonitor', UsePopupMonitor);
+    PopupTitleEqual := ReadBool('Popup', 'PopupTitleEqual', PopupTitleEqual);
+    PopupTitle := ini.ReadString('Popup', 'PopupTitle', PopupTitle);
+
+    PopupStyleCheck := ReadBool('Popup', 'PopupStyleCheck', PopupStyleCheck);
+    PopupStyleMask := ReadCardinal('Popup', 'PopupStyleMask', PopupStyleMask);
+    PopupStyleValue := ReadCardinal('Popup', 'PopupStyleValue', PopupStyleValue);
+
+    PopupExStyleCheck := ReadBool('Popup', 'PopupExStyleCheck', PopupExStyleCheck);
+    PopupExStyleMask := ReadCardinal('Popup', 'PopupExStyleMask', PopupExStyleMask);
+    PopupExStyleValue := ReadCardinal('Popup', 'PopupExStyleValue', PopupExStyleValue);
+  end;
+
+  ini.Free;
+end;
+
+{$IFDEF DEBUG}
+procedure LogSettings;
+begin
+  slDebug.Add('RunApp ' + Settings.RunApp);
+  slDebug.Add('Params ' + Settings.Params);
+  slDebug.Add('Folder ' + Settings.Folder);
+  slDebug.Add('Safe ' + BoolToStr(Settings.Safe, True));
+
+  slDebug.Add('StartTimeout ' + IntToStr(Settings.StartTimeout));
+  slDebug.Add('HideMonitorTimer ' + IntToStr(Settings.HideMonitorTimer));
+  slDebug.Add('PopupMonitorTimer ' + IntToStr(Settings.PopupMonitorTimer));
+
+  slDebug.Add('MainTitleEqual ' + BoolToStr(Settings.MainTitleEqual, True));
+  slDebug.Add('MainTitle ' + Settings.MainTitle);
+  slDebug.Add('IconTitle ' + Settings.IconTitle);
+  slDebug.Add('UpdateTitle ' + BoolToStr(Settings.UpdateTitle, True));
+  slDebug.Add('IconMain ' + Settings.IconMain);
+  slDebug.Add('IconPopup ' + Settings.IconPopup);
+
+  slDebug.Add('UsePopupMonitor ' + BoolToStr(Settings.UsePopupMonitor, True));
+  slDebug.Add('PopupTitleEqual ' + BoolToStr(Settings.PopupTitleEqual, True));
+  slDebug.Add('PopupTitle ' + Settings.PopupTitle);
+
+  slDebug.Add('PopupStyleCheck ' + BoolToStr(Settings.PopupStyleCheck, True));
+  slDebug.Add('PopupStyleMask ' + IntToHex(Settings.PopupStyleMask));
+  slDebug.Add('PopupStyleValue ' + IntToHex(Settings.PopupStyleValue));
+
+  slDebug.Add('PopupExStyleCheck ' + BoolToStr(Settings.PopupExStyleCheck, True));
+  slDebug.Add('PopupExStyleMask ' + IntToHex(Settings.PopupExStyleMask));
+  slDebug.Add('PopupExStyleValue ' + IntToHex(Settings.PopupExStyleValue));
+end;
+{$ENDIF}
 
 procedure ShowApp(wHandle: Cardinal);
 var
   style: Integer;
 begin
-  style := GetWindowLong(wHandle, GWL_EXSTYLE);
-  SetWindowLong(wHandle, GWL_EXSTYLE, style and (not WS_EX_TOOLWINDOW));
-  //Sleep(500);
+  if Settings.Safe then
+  begin
+    style := GetWindowLong(wHandle, GWL_EXSTYLE);
+    SetWindowLong(wHandle, GWL_EXSTYLE, style and (not WS_EX_TOOLWINDOW));
+  end
+  else
+    ShowWindow(wHandle, SW_SHOW);
+
   PostMessage(wHandle, WM_SYSCOMMAND, SC_RESTORE, 0);
 end;
 
@@ -123,7 +300,8 @@ begin
     slDebug.Add('[' + IntToStr(AppThread) + '] ' + IntToStr(wHandle) + ' "' + string(Title) + '"');
     {$ENDIF}
 
-    if (Trim(string(Title)) = '') then
+    if Settings.PopupTitleEqual and (Trim(string(Title)) = Settings.PopupTitle) or
+      (not Settings.PopupTitleEqual) and ((Settings.PopupTitle = '') or (Pos(Settings.PopupTitle, Trim(string(Title))) > 0)) then
     begin
       GetWindowInfo(wHandle, pwi);
 
@@ -131,13 +309,15 @@ begin
       slDebug.Add(IntToStr(pwi.dwStyle) + ' ' + IntToStr(pwi.dwExStyle) + ' ' + IntToStr(pwi.atomWindowType));
       {$ENDIF}
 
-      if (pwi.dwStyle and $FF000000 = $94000000) and (pwi.dwExStyle = $00080888) then
+      if ((not Settings.PopupStyleCheck) or (pwi.dwStyle and Settings.PopupStyleMask = Settings.PopupStyleValue)) and
+        ((not Settings.PopupExStyleCheck) or (pwi.dwExStyle and Settings.PopupExStyleMask = Settings.PopupExStyleValue)) then
       begin
-        frmMain.TrayIcon.IconIndex := 1;
+        PopupDetected := True;
         Result := False;
       end;
     end;
   end;
+
   {$IFDEF DEBUG}
   try
     slDebug.SaveToFile('D:\log.log');
@@ -159,8 +339,13 @@ begin
     slDebug.SaveToFile('D:\log.log');
     {$ENDIF}
 
-    if Pos('Mozilla Thunderbird', string(Title)) > 0 then
+    if Settings.MainTitleEqual and (Trim(string(Title)) = Settings.MainTitle) or
+      (not Settings.MainTitleEqual) and ((Settings.MainTitle = '') or (Pos(Settings.MainTitle, Trim(string(Title))) > 0)) then
+    begin
       WinHandle := wHandle;
+      if Settings.IconTitle = '' then
+        Settings.IconTitle := string(Title);
+    end;
   end;
 
   Result := WinHandle = 0;
@@ -176,11 +361,24 @@ end;
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   Screen.Cursor := crAppStart;
+  WinHandle := 0;
+
   {$IFDEF DEBUG}
   slDebug := TStringList.Create;
   {$ENDIF}
 
-  WinHandle := 0;
+  InitDefaultSettings;
+  InitSettings;
+
+  {$IFDEF DEBUG}
+  LogSettings;
+  {$ENDIF}
+
+  if Settings.RunApp = '' then
+  begin
+    Application.Terminate;
+    Exit;
+  end;
 
   RunApp;
 
@@ -192,7 +390,9 @@ begin
 
   Self.Hide;
   Screen.Cursor := crDefault;
+  TrayIcon.Hint := Settings.IconTitle;
 
+  Timer.Interval := Settings.HideMonitorTimer;
   Timer.OnTimer := HideTimer;
   Timer.Enabled := True;
 end;
@@ -205,6 +405,9 @@ begin
 end;
 
 procedure TfrmMain.HideTimer(Sender: TObject);
+var
+  Title: array[0..255] of char;
+  aTitle: string;
 begin
   if not IsWindow(WinHandle) then
   begin
@@ -212,14 +415,113 @@ begin
     Application.Terminate;
   end;
 
+  if Settings.UpdateTitle then
+  begin
+    GetWindowText(WinHandle, Title, 255);
+    aTitle := string(Title);
+    if TrayIcon.Hint <> aTitle then
+      TrayIcon.Hint := aTitle;
+  end;
+
   if IsIconic(WinHandle) then
   begin
     Timer.Enabled := False;
     HideApp(WinHandle);
+    miShowHide.Caption := 'Show';
 
-    Timer.Interval := 2000;
-    Timer.OnTimer := MonitorTimer;
-    Timer.Enabled := True;
+    if Settings.UsePopupMonitor then
+    begin
+      Timer.Interval := Settings.PopupMonitorTimer;
+      Timer.OnTimer := MonitorTimer;
+      Timer.Enabled := True;
+    end;
+  end;
+end;
+
+procedure TfrmMain.InitSettings;
+var
+  iIniFile: string;
+  anIcon: TIcon;
+  i, startIdx: Integer;
+begin
+  if AnsiLowerCase(ExtractFileExt(ParamStr(1))) = '.ini' then
+    startIdx := 2
+  else
+    startIdx := 1;
+
+  iIniFile := ChangeFileExt(Application.ExeName, '.ini');
+  if startIdx > 1 then
+    iIniFile := ParamStr(1);
+  iIniFile := AddPath(iIniFile);
+
+  if FileExists(iIniFile) then
+    LoadSettings(iIniFile);
+
+  if AnsiLowerCase(ParamStr(1)) = '/safe' then
+  begin
+    Settings.Safe := True;
+    startIdx := 2;
+  end;
+
+  if (ParamCount >= startIdx) and FileExists(AddPath(ParamStr(startIdx))) then
+  begin
+    Settings.RunApp := AddPath(ParamStr(startIdx));
+    Inc(startIdx);
+  end;
+
+  for i := startIdx to ParamCount do
+    Settings.Params := Settings.Params + ' ' + ParamStr(i);
+  Settings.Params := Trim(Settings.Params);
+
+  if Settings.IconMain <> '' then
+  begin
+    ImageList.Clear;
+    anIcon := TIcon.Create;
+    anIcon.LoadFromFile(Settings.IconMain);
+    ImageList.AddIcon(anIcon);
+
+    if Settings.IconPopup <> '' then
+    begin
+      anIcon := TIcon.Create;
+      anIcon.LoadFromFile(Settings.IconPopup);
+      ImageList.AddIcon(anIcon);
+    end;
+  end
+  else
+  if (Settings.RunApp <> '') and FileExists(Settings.RunApp) then
+  begin
+    anIcon := TIcon.Create;
+    anIcon.Handle := ExtractIcon(HInstance, PWideChar(Settings.RunApp), 0);
+    if anIcon.Handle > 0 then
+    begin
+      ImageList.Delete(0);
+      ImageList.InsertIcon(0, anIcon);
+    end;
+  end;
+
+  TrayIcon.Icons := ImageList;
+  TrayIcon.IconIndex := -1; // hack
+
+  TrayIcon.Visible := True;
+  TrayIcon.IconIndex := 0;
+end;
+
+procedure TfrmMain.miExitClick(Sender: TObject);
+begin
+  if IsIconic(WinHandle) then
+    ShowApp(WinHandle);
+  PostMessage(WinHandle, WM_CLOSE, 0, 0);
+  Application.Terminate;
+end;
+
+procedure TfrmMain.miShowHideClick(Sender: TObject);
+begin
+  if IsIconic(WinHandle) then
+    TrayIconClick(Sender)
+  else
+  begin
+    PostMessage(WinHandle, WM_SYSCOMMAND, SC_MINIMIZE, 0);
+    HideTimer(Sender);
   end;
 end;
 
@@ -231,27 +533,33 @@ begin
     Application.Terminate;
   end;
 
+  PopupDetected := False;
   EnumThreadWindows(AppThread, @FindPopupNotificationProc, 0);
-  if TrayIcon.IconIndex = 1 then Timer.Enabled := False;
+
+  if PopupDetected then
+  begin
+    Timer.Enabled := False;
+    TrayIcon.IconIndex := 1;
+  end;
 end;
 
 procedure TfrmMain.RunApp;
 var
   ErrorCode: Integer;
-  Path: string;
   i: Integer;
 begin
-  Path := ExtractFilePath(Application.ExeName) + 'thunderbird.exe';
-
-  if not FileExists(Path) then
-    Path := 'C:\Program Files\Mozilla Thunderbird\thunderbird.exe';
-
-  if not FileExists(Path) then
+  if not FileExists(Settings.RunApp) then
     Exit;
 
-  AppThread := InstExec(Path, '', '', SW_NORMAL, ErrorCode);
+  AppThread := ExecuteApp(Settings.RunApp, Settings.Params, Settings.Folder, SW_NORMAL, ErrorCode);
 
-  for i := 0 to 20 do
+  if AppThread = LAUNCH_ERROR then
+  begin
+    MessageBox(Handle, PWideChar('Error running ' + Settings.RunApp), PWideChar('Error'), MB_ICONERROR);
+    Exit;
+  end;
+
+  for i := 0 to Settings.StartTimeout * 2 do
   begin
     Sleep(500);
     EnumThreadWindows(AppThread, @FindWindowHandleProc, 0);
@@ -271,9 +579,10 @@ begin
   Timer.Enabled := False;
 
   ShowApp(WinHandle);
-  frmMain.TrayIcon.IconIndex := 0;
+  TrayIcon.IconIndex := 0;
+  miShowHide.Caption := 'Hide';
 
-  Timer.Interval := 500;
+  Timer.Interval := Settings.HideMonitorTimer;
   Timer.OnTimer := HideTimer;
   Timer.Enabled := True;
 end;
